a2155b280b607209bc6f38b2751c67a6c302c4f0
sbublie <59267927+sbublie@users.noreply.github.com>
2024-11-05 21:02:28 +0100

    Avoid multiple levels of indentation

--- src/main/java/com/cgi/tennis/TennisGameRefactored.java
+++ src/main/java/com/cgi/tennis/TennisGameRefactored.java
@@ -1,7 +1,9 @@
 package com.cgi.tennis;
 
+
+// Avoid multiple levels of indentation
 public class TennisGameRefactored implements TennisGame {
-    
+
     private int p2;
     private int p1;
     private String p1N;
@@ -13,25 +15,34 @@ public class TennisGameRefactored implements TennisGame {
     }
 
     public String getScore() {
-        String s;
+
         if (p1 < 4 && p2 < 4 && !(p1 + p2 == 6)) {
-            String[] p = new String[]{"Love", "Fifteen", "Thirty", "Forty"}; 
-            s = p[p1];
-            return (p1 == p2) ? s + "-All" : s + "-" + p[p2];
+            return getRegularResult();
+        } else if (p1 == p2) {
+            return "Deuce";
         } else {
-            if (p1 == p2)
-                return "Deuce";
-            s = p1 > p2 ? p1N : p2N;
-            return ((p1-p2)*(p1-p2) == 1) ? "Advantage " + s : "Win for " + s;
+            return getAdvantageOrWin();
         }
     }
-    
+
+    private String getAdvantageOrWin() {
+        String s;
+        s = p1 > p2 ? p1N : p2N;
+        return ((p1-p2)*(p1-p2) == 1) ? "Advantage " + s : "Win for " + s;
+    }
+
+    private String getRegularResult() {
+        String s;
+        String[] p = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
+        s = p[p1];
+        return (p1 == p2) ? s + "-All" : s + "-" + p[p2];
+    }
+
     public void wonPoint(String playerName) {
         if (playerName == this.p1N)
             this.p1 += 1;
         else
             this.p2 += 1;
-        
-    }
 
+    }
 }
